84d80e06c25e88d7fbc44530dc472bc960ee078e
Dhruvdot7 <dhruvguptag7@gmail.com>
2026-04-01 10:28:03 +0530

    Day 19

---
+++ Day_19/Dhruv/lc643.java
@@ -0,0 +1,16 @@
+// Leetcode Problem 643. Maximum Average Subarray I
+
+public class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int max_sum = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            max_sum = Math.max(max_sum, sum);
+        }
+        return (double) max_sum / k;
+    }
+}
