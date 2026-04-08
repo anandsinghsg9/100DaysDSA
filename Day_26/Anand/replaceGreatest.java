fbd770040a12ca5b2c4069e5c7857d468e64799d
Anand Singh <anandsinghsg9@gmail.com>
2026-04-08 23:16:17 +0530

    Day-26.replaceGreatest

---
+++ Day_26/Anand/replaceGreatest.java
@@ -0,0 +1,21 @@
+package Day_26.Anand;
+import java.util.Arrays;
+
+public class replaceGreatest {
+    public int[] replaceElements(int[] arr) {
+        int max=arr[arr.length-1];
+        int []output= new int [arr.length];
+        for(int i=arr.length-1;i>0;i--){
+            max=Math.max(max,arr[i]);
+            output[i-1]=max;
+        }
+        output[arr.length-1]=-1;
+        return output;
+    }
+    public static void main(String[] args) {
+        int [] arr={17,18,5,4,6,1};
+        replaceGreatest sol=new replaceGreatest();
+        System.out.println(Arrays.toString(sol.replaceElements(arr)));
+    }
+    
+}
