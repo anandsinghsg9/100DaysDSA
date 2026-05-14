161519a193c782d8db274c89a706f2308879ccbe
anushhka sharma <anushkasharma2386@gmail.com>
2026-05-14 11:52:11 +0530

    singleBetweenDoubles
    finds the single element in a sorted array where every other element appears twice.

---
+++ Day-48/Anushka/singleBetweenDoubles.java
@@ -0,0 +1,51 @@
+
+public class singleBetweenDoubles {
+
+    // Method to find single element
+    static int single(int[] arr) {
+
+        int n = arr.length;
+
+        if (n == 1)
+            return arr[0];
+
+        if (arr[0] != arr[1])
+            return arr[0];
+
+        if (arr[n - 1] != arr[n - 2])
+            return arr[n - 1];
+
+        int lo = 0, hi = n - 1;
+
+        while (lo <= hi) {
+
+            int mid = (lo + hi) / 2;
+
+            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
+                return arr[mid];
+
+            int f = mid, s = mid;
+
+            if (arr[mid] == arr[mid - 1])
+                f = mid - 1;
+            else
+                s = mid + 1;
+
+            if ((f - lo) % 2 == 0)
+                lo = s + 1;
+            else
+                hi = f - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 2, 1, 4, 4, 8, 8, 9, 9};
+
+        int ans = single(arr);
+
+        System.out.println("Single Element = " + ans);
+    }
+}
