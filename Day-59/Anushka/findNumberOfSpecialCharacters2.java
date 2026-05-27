f3a3e550821e55096302719c962c11b898f05500
anushhka sharma <anushkasharma2386@gmail.com>
2026-05-27 22:28:23 +0530

    numberOfSpecialChars
    This program counts special characters in a string where both lowercase and uppercase letters exist, and all lowercase letters appear before the uppercase letter.
    
    It checks every letter from a to z using lastIndexOf() and indexOf() to verify the condition.

---
+++ Day-59/Anushka/findNumberOfSpecialCharacters2.java
@@ -0,0 +1,27 @@
+public class findNumberOfSpecialCharacters2 {
+    public static int numberOfSpecialChars(String word) {
+        int count = 0;
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+
+            int small = word.lastIndexOf(ch);
+
+            int capital = word.indexOf(Character.toUpperCase(ch));
+
+            if(small != -1 &&
+               capital != -1 &&
+               small < capital) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        String word = "aaAbcBC";
+
+        System.out.println(numberOfSpecialChars(word));
+    }
+}
