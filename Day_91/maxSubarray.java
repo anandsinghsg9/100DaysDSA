fa7f04ea9603fd6f9a22925802a37a69b6c5499b
Anand Singh <anandsinghsg9@gmail.com>
2026-08-13 20:08:56 +0530

    Day_91.maxSubarray

---
+++ Day_91/maxSubarray.java
@@ -0,0 +1,37 @@
+package Day_91;
+
+import java.util.HashMap;
+
+public class maxSubarray {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int left=0;
+        int right=k-1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int size=0;
+        for(int i=0;i<k;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            size++;
+        }
+        for(int i=k;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            right++;
+            if(map.get(nums[i])>k){
+                while(nums[left]!=nums[i]){
+                    map.put(nums[left],map.get(nums[left])-1);
+                    left++;
+                }
+                map.put(nums[left],map.get(nums[left])-1);
+                left++;
+            }
+            int step=right-left+1;
+            size=Math.max(step,size);
+        }
+        return size;
+    }
+    public static void main(String[] args) {
+        maxSubarray obj= new maxSubarray();
+        int []nums={1,2,3,1,2,3,1,2};
+        int k=2;
+        System.out.println(obj.maxSubarrayLength(nums, k));
+    }
+}
