7424c5680203280195c825fa2852d53ba4cd6940
Anand Singh <anandsinghsg9@gmail.com>
2026-09-02 02:02:18 +0530

    Day_96.longestSubarray

---
+++ Day_96/Anand/maxSubarray.java
@@ -0,0 +1,42 @@
+package Day_96.Anand;
+
+public class maxSubarray {
+    public int longestSubarray(int[] nums) {
+        int temp=0;
+        int left=0;
+        int right=0;
+        int result=right-left;
+        while(right<nums.length){
+            if(nums[right]==1){
+                right++;
+            }
+            else{
+                if(temp<1){
+                    temp++;
+                    right++;
+                }
+                else{
+                    while(temp>0){
+                        if(nums[left]==0){
+                            temp--;
+                        }
+                        left++;
+                    }
+                    right++;
+                    temp++;
+                }
+            }
+            int step=right-left;
+            result=Math.max(step,result);
+        }
+        if(nums.length==result){
+            return result-1;
+        }
+        return result-temp;
+    }
+    public static void main(String[] args) {
+        maxSubarray obj=new maxSubarray();
+        int [] nums = {1,1,0,1};
+        System.out.print(obj.longestSubarray(nums));
+    }
+}
