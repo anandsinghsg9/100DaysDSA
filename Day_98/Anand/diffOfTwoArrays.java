58a8426202cd8c5c628cd5ee129472a6913f9d11
Anand Singh <anandsinghsg9@gmail.com>
2026-09-11 00:28:19 +0530

    Day_98.findDifferance

---
+++ Day_98/Anand/diffOfTwoArrays.java
@@ -0,0 +1,42 @@
+package Day_98.Anand;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class diffOfTwoArrays {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1=new HashSet<>();
+        HashSet<Integer> set2=new HashSet<>();
+        for(int i=0;i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
+        for(int j=0;j<nums2.length;j++){
+            set2.add(nums2[j]);
+        }
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> row1=new ArrayList<>();
+        List<Integer> row2=new ArrayList<>();
+        for(int k=0;k<nums1.length;k++){
+            if(!set2.contains(nums1[k])){
+                row1.add(nums1[k]);
+                set2.add(nums1[k]);
+            }
+        }
+        for(int l=0;l<nums2.length;l++){
+            if(!set1.contains(nums2[l])){
+                row2.add(nums2[l]);
+                set1.add(nums2[l]);
+            }
+        }
+        list.add(row1);
+        list.add(row2);
+        return list;
+    }
+    public static void main(String[] args) {
+        diffOfTwoArrays obj =new diffOfTwoArrays();
+        int []nums1 = {1,2,3}; 
+        int[]nums2 = {2,4,6};
+        System.out.println(obj.findDifference(nums1, nums2));
+    }
+}
